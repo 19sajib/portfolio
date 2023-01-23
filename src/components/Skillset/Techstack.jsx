7bb9551b545f87de9ec037e8acb523396add2583
Sajib <contact2sajib@gmail.com>
2023-01-23 17:48:05 +0600

    Techstack & Toolstack

---
+++ src/components/Skillset/Techstack.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiGit,
+} from "react-icons/di";
+import {
+  SiMaterialui,
+  SiGraphql,
+  SiSolidity,
+  SiRedux,
+  SiMongodb,
+  SiHtml5
+} from "react-icons/si";
+
+const Techstack = () => {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRedux />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGraphql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSolidity />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMaterialui />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
---
+++ src/components/Skillset/Toolstack.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiHeroku,
+  SiVercel,
+  SiNetlify
+} from "react-icons/si";
+
+const Toolstack = () => {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVercel />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNetlify />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHeroku />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
