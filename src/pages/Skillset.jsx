75421f9f6bb90edc8506b4f17a13b8a5cf682a96
Sajib <contact2sajib@gmail.com>
2023-01-23 18:14:57 +0600

    Skillset

--- src/pages/Skillset.jsx
+++ src/pages/Skillset.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
+
+import Particle from '../components/Particle'
+import Techstack from "../components/Skillset/Techstack";
+import Toolstack from "../components/Skillset/Toolstack";
+import Leetcode from "../components/Skillset/Leetcode";
+import Github from "../components/Skillset/Github";
 
 const Skillset = () => {
   return (
-    <div>Skillset</div>
+    <Container fluid className="about-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          Professional <strong className="yellow">Skillset </strong>
+        </h1>
+
+        <Techstack />
+
+        <h1 className="project-heading">
+          <strong className="yellow">Tools</strong> I use
+        </h1>
+        <Toolstack />
+
+        <Leetcode />
+        <Github />
+      </Container>
+    </Container>
   )
 }
 
