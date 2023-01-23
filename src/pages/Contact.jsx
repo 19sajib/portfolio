389d25ba366b345255a56fbf916b4d7bd46a6608
Sajib <contact2sajib@gmail.com>
2023-01-23 22:05:52 +0600

    Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,69 @@
 import React from 'react'
+import { Container, Row, Col } from "react-bootstrap";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import Particle from '../components/Particle'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <Container style={{padding: '60px'}}>
+      <Particle />
+    <Row>
+              <Col md={12} className="home-about-social">
+                <h1>FIND ME ON</h1>
+                <p>
+                  Feel free to <span className="yellow">connect </span>with me
+                </p>
+                <ul className="home-about-social-links">
+                  <li className="social-icons">
+                    <a
+                      href="https://github.com/19sajib"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                    >
+                      <AiFillGithub />
+                    </a>
+                  </li>
+                  <li className="social-icons">
+                    <a
+                      href="https://twitter.com/19sajib"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                    >
+                      <AiOutlineTwitter />
+                    </a>
+                  </li>
+                  <li className="social-icons">
+                    <a
+                      href="https://www.linkedin.com/in/19sajib/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                    >
+                      <FaLinkedinIn />
+                    </a>
+                  </li>
+                  <li className="social-icons">
+                    <a
+                      href="https://www.instagram.com/19sajib"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour home-social-icons"
+                    >
+                      <AiFillInstagram />
+                    </a>
+                  </li>
+                </ul>
+              </Col>
+            </Row>
+          </Container>
   )
 }
 
