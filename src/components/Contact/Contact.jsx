916fa04019d061bef2c7126058fd1507dc5ad3e1
Sajib <contact2sajib@gmail.com>
2023-04-12 23:08:09 +0600

    contact mail dummy msg

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -59,7 +59,7 @@ const Contact = () => {
                 <textarea name="message" className="user" placeholder="Message" onChange={handleChange} />
                 <span className='not-done' >{notDone && "Please, fill all the input field"}</span>
                 <Button type="submit" className="button" disabled={done}>Send</Button>
-                <span className='done'>{done && "Thanks for Contacting me"}</span>
+                <span className='done'>{done && "Thanks for contacting me and be sure i have recieved your mail. If you are testing this functionality then i am confirming this thing working perfectly fine. If you have any serious query then i will reply. Also if you need me, you can conatct me on Linkedin."}</span>
                 </form>
             </Col>
             </Row>
